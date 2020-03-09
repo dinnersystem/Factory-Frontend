e0efd8e934043bb1db2ec9dcad7762cc86ada24a
lawrence910426 <lawrence910426@gmail.com>
2020-03-09 14:36:38 +0800

    Update again

--- FactoryClient/FactoryClient/Login.designer.cs
+++ FactoryClient/FactoryClient/Login.designer.cs
@@ -35,21 +35,21 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.password = new System.Windows.Forms.MaskedTextBox();
-            this.Saver = new System.Windows.Forms.LinkLabel();
-            this.SaveMe = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // id
             // 
             this.id.ImeMode = System.Windows.Forms.ImeMode.Alpha;
-            this.id.Location = new System.Drawing.Point(85, 67);
+            this.id.Location = new System.Drawing.Point(85, 90);
             this.id.Name = "id";
             this.id.Size = new System.Drawing.Size(131, 22);
             this.id.TabIndex = 0;
             // 
             // login_btn
             // 
-            this.login_btn.Location = new System.Drawing.Point(85, 123);
+            this.login_btn.Location = new System.Drawing.Point(85, 146);
             this.login_btn.Name = "login_btn";
             this.login_btn.Size = new System.Drawing.Size(97, 23);
             this.login_btn.TabIndex = 3;
@@ -60,7 +60,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 70);
+            this.label1.Location = new System.Drawing.Point(36, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(29, 12);
             this.label1.TabIndex = 3;
@@ -69,7 +69,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 98);
+            this.label2.Location = new System.Drawing.Point(36, 121);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 4;
@@ -79,49 +79,47 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("PMingLiU", 15F);
-            this.label3.Location = new System.Drawing.Point(66, 33);
+            this.label3.Location = new System.Drawing.Point(81, 28);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 20);
+            this.label3.Size = new System.Drawing.Size(89, 20);
             this.label3.TabIndex = 5;
-            this.label3.Text = "廠商管理前端";
+            this.label3.Text = "廠商管理";
             // 
             // password
             // 
-            this.password.Location = new System.Drawing.Point(85, 95);
+            this.password.Location = new System.Drawing.Point(85, 118);
             this.password.Name = "password";
             this.password.PasswordChar = '*';
             this.password.Size = new System.Drawing.Size(131, 22);
             this.password.TabIndex = 2;
             // 
-            // Saver
+            // label4
             // 
-            this.Saver.AutoSize = true;
-            this.Saver.Font = new System.Drawing.Font("PMingLiU", 30F);
-            this.Saver.Location = new System.Drawing.Point(50, 226);
-            this.Saver.Name = "Saver";
-            this.Saver.Size = new System.Drawing.Size(177, 40);
-            this.Saver.TabIndex = 7;
-            this.Saver.TabStop = true;
-            this.Saver.Text = "問題回覆";
-            this.Saver.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.Saver_LinkClicked);
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "學校";
             // 
-            // SaveMe
+            // comboBox1
             // 
-            this.SaveMe.Location = new System.Drawing.Point(85, 152);
-            this.SaveMe.Name = "SaveMe";
-            this.SaveMe.Size = new System.Drawing.Size(131, 23);
-            this.SaveMe.TabIndex = 8;
-            this.SaveMe.Text = "系統掛了，快來救我";
-            this.SaveMe.UseVisualStyleBackColor = true;
-            this.SaveMe.Click += new System.EventHandler(this.SaveMe_Click);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "新北市立板橋高中"});
+            this.comboBox1.Location = new System.Drawing.Point(85, 63);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(131, 20);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Text = "不用選學校沒關係";
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(268, 186);
-            this.Controls.Add(this.SaveMe);
-            this.Controls.Add(this.Saver);
+            this.ClientSize = new System.Drawing.Size(268, 202);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.password);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -130,7 +128,7 @@
             this.Controls.Add(this.id);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
-            this.Text = "幹";
+            this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,7 +142,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.MaskedTextBox password;
-        private System.Windows.Forms.LinkLabel Saver;
-        private System.Windows.Forms.Button SaveMe;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
