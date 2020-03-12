c70e92f3b1c3bd550e264ebaf641b6099cf60592
lawrence910426 <lawrence910426@gmail.com>
2020-03-12 22:10:51 +0800

    This might going to fail

--- FactoryClient/FactoryClient/Base_Function/ExcelStream.cs
+++ FactoryClient/FactoryClient/Base_Function/ExcelStream.cs
@@ -7,6 +7,9 @@ using System.Diagnostics;
 using Excel = Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
 using System.Threading;
+using NPOI.XSSF.UserModel;
+using System.IO;
+using NPOI.SS.UserModel;
 
 namespace FactoryClient
 {
@@ -47,14 +50,7 @@ namespace FactoryClient
                 Sheet.get_Range("A2:ZZ999", Type.Missing).Cells.Value2 = "";
             }
         }
-        ~ExcelStream()
-        {
-            try
-            {
-                Close();
-            }
-            catch (Exception e) { }
-        }
+        ~ExcelStream() { try { Close(); } catch (Exception e) { } }
 
         public void Close()
         {
@@ -67,10 +63,7 @@ namespace FactoryClient
             Thread.Sleep(1000);
         }
 
-        public void Write(int x, int y, object value)
-        {
-            Sheet.Cells[x, y] = value;
-        }
+        public void Write(int x, int y, object value) { Sheet.Cells[x, y] = value; }
 
         public void PageBreak(int rows)
         {
@@ -97,6 +90,109 @@ namespace FactoryClient
                     row.Add(value);
                 }
             }
+            return ret;
+        }
+    }
+
+
+
+
+
+    class ExcelStream
+    {
+        string path;
+        public ExcelStream(string path, bool reset = true)
+        {
+            Excel.Application _Excel;
+            bool flag = false;
+            foreach (var item in Process.GetProcesses())
+            {
+                if (item.ProcessName == "EXCEL")
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+            {
+                _Excel = new Excel.Application();
+            }
+            else
+            {
+                object obj = Marshal.GetActiveObject("Excel.Application");//引用已在執行的Excel
+                _Excel = obj as Excel.Application;
+            }
+            _Excel.Visible = false;//設false效能會比較好
+            Book = _Excel.Workbooks.Open(path);
+            Sheet = Book.Sheets[1];
+
+            /* Reset the file */
+            if (reset)
+            {
+                _Excel.ActiveSheet.ResetAllPageBreaks();
+                Sheet.get_Range("A2:ZZ999", Type.Missing).Cells.Value2 = "";
+            }
+        }
+        ~ExcelStream() { try { Close(); } catch (Exception e) { } }
+
+        public void Close()
+        {
+            Book.Save();
+            Process[] procs = Process.GetProcessesByName("excel");
+            foreach (Process pro in procs)
+            {
+                pro.Kill();//沒有更好的方法,只有殺掉進程
+            }
+            Thread.Sleep(1000);
+        }
+
+        public void Write(int x, int y, object value)
+        {
+            using (FileStream rstr = new FileStream(file, FileMode.Open, FileAccess.Read))
+            {
+                IWorkbook workbook = new XSSFWorkbook(rstr);
+                ISheet sheet = workbook.GetSheetAt(0);
+
+                using (FileStream wstr = new FileStream(file, FileMode.Open, FileAccess.ReadWrite))
+                {
+                    string secondValue = "changes";
+
+                    row = sheet.GetRow(x); cell = row.GetCell(y); cell.SetCellValue(secondValue);
+                    Debug.Print(cell.ToString());
+                    workbook.Write(wstr);
+                    wstr.Close();
+                }
+                rstr.Close();
+            }
+        }
+
+        public void PageBreak(int rows)
+        {
+            if (rows == 1) return;
+            Sheet.HPageBreaks.Add(Sheet.Range["A" + rows.ToString()]);
+        }
+
+        public List<List<string>> ReadExcel()
+        {
+            XSSFWorkbook workbook = new XSSFWorkbook(path);
+            XSSFSheet sheet = (XSSFSheet)workbook.GetSheetAt(0);
+            List<List<string>> ret = new List<List<string>>();
+            for (int i = (sheet.FirstRowNum + 1); i < sheet.LastRowNum + 1; i++)
+            {
+                XSSFRow row = (XSSFRow)sheet.GetRow(i); List<string> row_ans = new List<string>();
+                try
+                {
+                    for (int j = row.FirstCellNum; j < ((XSSFRow)sheet.GetRow(0)).LastCellNum; j++)
+                        if (null != row.GetCell(j) && !string.IsNullOrEmpty(row.GetCell(j).ToString()) && !string.IsNullOrWhiteSpace(row.GetCell(j).ToString()))
+                            row_ans.Add(row.GetCell(j).ToString());
+
+                }
+                catch (Exception Ex) { }
+                finally { if (row_ans.Count != 0) ret.Add(row_ans); }
+            }
+            workbook.UnlockStructure(); workbook.UnlockRevision(); workbook.UnlockWindows();
+            workbook = null; sheet = null;
             return ret;
         }
     }
